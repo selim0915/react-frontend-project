59e7e54d5194a8424ad6113c2ca3dc4029550749
srwoo <rhak12@naver.com>
2023-12-27 20:17:42 +0900

    next/link

--- toy-app/pages/launches.jsx
+++ toy-app/pages/launches.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react'
 
-export default function Launches() {
-    const [data, setData] = useState(null);
+export default function Launches({ data }) {
+    // const [data, setData] = useState(null);
 
-    useEffect(()=>{
-        const feactLaunches = async () => {
-            const res = await fetch('https://api.spacexdata.com/v3/launches');
-            const data = await res.json();
+    // useEffect(()=>{
+    //     const feactLaunches = async () => {
+    //         const res = await fetch('https://api.spacexdata.com/v3/launches');
+    //         const data = await res.json();
 
-            setData(data);
+    //         setData(data);
             
-        }
-        feactLaunches();
-    }, [])
+    //     }
+    //     feactLaunches();
+    // }, [])
 
     if(data === null){
         return null;
@@ -20,11 +20,20 @@ export default function Launches() {
 
     return (
         <div>
-            <ul>
+            <ol>
                 {data.map((item, index) => (
                     <li key={index}>{item.mission_name}</li>
                 ))}
-            </ul>
+            </ol>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch('https://api.spacexdata.com/v3/launches');
+    const data = await res.json();
+
+    return { 
+        props: { data }
+    }
 }
---
+++ toy-app/pages/posts/first-post.jsx
@@ -0,0 +1,5 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return <Link href="/">Back to home</Link>;
+}
