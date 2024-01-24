02cf492e57cef4a81fb792dc438773ec51a43d62
HEAD <rhak12@naver.com>
2024-01-24 23:26:46 +0900

    [PAS-19] eslint, prettier

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # react-frontend-project
 
-Incorporating various technologies into React
+<br/>
+
+## Node Version
+
+### 18.19.0
+
+<br/>
+## How to run
+### npm run test
+### npm run start
+### npm run build
+### npm run lint
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
 
 const App = () => {
   console.log(process.env.NODE_ENV);
   // eslint-disable-next-line no-undef
   console.log(TWO);
   // eslint-disable-next-line no-undef
-  console.log(api.domain)
+  console.log(api.domain);
 
-  const alert = msg => window.alert(msg);
+  const alert = (msg) => window.alert(msg);
 
-  return(
+  return (
     <div>
       <h1>Hello World??</h1>
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
