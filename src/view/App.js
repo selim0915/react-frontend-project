fe371954ae13fe37bb084cd6475c41881a100c0a
srwoo <srwoo@hyundaiht.co.kr>
2024-01-25 17:32:23 +0900

    .

--- src/view/App.js
+++ src/view/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+
 const App = () => {
   return (
     <div>
       <h1>Hello World??</h1>
+      <img src='../asset/png/netflix.png' alt='img' width={100} height={50} />
     </div>
   );
 };
