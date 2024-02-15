1b4abacd661ccbdbbbbb2a28d7ff300314c5622a
HEAD <rhak12@naver.com>
2024-02-16 00:16:27 +0900

    axios 테스트

--- react-frontend-project/src/components/board/Board.tsx
+++ react-frontend-project/src/components/board/Board.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Board: React.FC = () => {
+  const [productList, setProductList] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios
+      .get('/product/list')
+      .then(function (res) {
+        setProductList(res.data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <>
       <h1>Board</h1>
+      {productList.map((item: any, index: any) => {
+        return (
+          <ul key={index}>
+            <li>{item.name}</li>
+            <li>{item.description}</li>
+            <li>{item.price}</li>
+          </ul>
+        );
+      })}
     </>
   );
 };
