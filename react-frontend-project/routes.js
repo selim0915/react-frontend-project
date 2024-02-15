44f9823a025a160044eb01edd072c24b65bee648
HEAD <rhak12@naver.com>
2024-02-15 23:41:01 +0900

    route url 요청시 연결안되는 오류 수정 - 라우터 js 작성 순서에따라서 매핑을 확인하기 때문에 '*' 경로의 경우 해당 라우터만 바라보게 된다

--- react-frontend-project/routes.js
+++ react-frontend-project/routes.js
@@ -2,46 +2,11 @@ const path = require('path');
 const productController = require('./controller/products');
 
 function initialize(app) {
-  app.get('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'), {}, function (err) {
-      if (err) {
-        console.error('err', err);
-        res.sendStatus(err.status).end();
-      }
-    });
-  });
-
-  app.get('/src/images/ST/*', (req, res) => {
-    let _path = req.path;
-    let _pathPosition = _path.lastIndexOf('/') + 1;
-    // let _pathList = _path.split('/');
-    let _fileName = _path.substring(_pathPosition);
-    let _ST_filePath = path.join(__dirname, 'src/images/ST');
-    // let _ST_filePath2 = _pathList[_pathList.length - 2] !== 'ST' ? _pathList[_pathList.length - 2] : '';
-    // res.sendFile(path.join(_ST_filePath, _ST_filePath2, _fileName), {}, (err) => {
-    res.sendFile(path.join(_ST_filePath, _fileName), {}, (err) => {
+  app.get('/hello', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/images/ST/netflix.png'), {}, (err) => {
       if (err) {
         console.error('err', err);
         res.status(404).send('no such file or directory.');
-      }
-    });
-  });
-
-  app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'), {}, function (err) {
-      if (err) {
-        console.error('err', err);
-        res.sendStatus(err.status).end();
-      }
-    });
-  });
-
-  app.get('/logout', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'), {}, function (err) {
-      if (err) {
-        console.error('err', err);
-        res.sendStatus(err.status).end();
       }
     });
   });
@@ -51,6 +16,15 @@ function initialize(app) {
   app.get('/:productId', productController.getProductById);
   app.put('/:productId', productController.updateProduct);
   app.delete('/:productId', productController.deleteProduct);
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'), {}, function (err) {
+      if (err) {
+        console.error('err', err);
+        res.sendStatus(err.status).end();
+      }
+    });
+  });
 
   app.use((error, req, res, next) => {
     res.status(500).json({ message: error.message });
--- react-frontend-project/server.js
+++ react-frontend-project/server.js
@@ -7,36 +7,17 @@ const db = require('./db');
 const PORT = process.env.APP_PORT || 3001;
 const ROOT = path.resolve(__dirname, 'dist');
 
-// express
 const app = express();
+
+// webpack
+app.use(express.static(ROOT));
 
 // route
 const routes = require('./routes');
-
-// passport
-// const passport = require('passport');
-// const passportConfig = require('./passport.config');
-// passportConfig();
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// webpack
-// const webpack = require('webpack');
-// const webpackConfig = require('./webpack.config.js');
-// const compiler = webpack(webpackConfig);
-// app.use(require('webpack-dev-middleware')(compiler, webpackConfig.devServer));
-// app.use('/src/images/ST', express.static(path.join(__dirname, 'src', 'images', 'ST')));
-// app.use('/src/images/ST', express.static('\Program Files\Git\app\react-frontend-project\src\images\ST'));
-// app.use(
-//   '/src/images/ST',
-//   express.static(path.join(__dirname, '/Program Files/Git/app/react-frontend-project/src/images/ST'))
-// );
-
-app.use(express.static(ROOT));
+routes.initialize(app);
 
 app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
-  routes.initialize(app);
 });
