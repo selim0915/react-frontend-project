ef10368cd117feabc5509863e2d0fcca1e9a5e29
srwoo <srwoo@hyundaiht.co.kr>
2025-04-02 16:17:58 +0900

    fix. socket 연결 로직 개선 및 iconv-lite 추가로 디코딩 수정

--- front/src/pages/admin/index.tsx
+++ front/src/pages/admin/index.tsx
@@ -3,47 +3,66 @@ import React, { useEffect, useState } from 'react';
 const Admin: React.FC = () => {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
-  const [isWs, setisWs] = useState<WebSocket | null>(null);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:3001');
-    setisWs(ws);
-
-    ws.onmessage = (event) => {
-      setOutput(event.data);
-    };
-  }, []);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  };
+    return () => socket?.close();
+  }, [socket]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (isWs) {
-      isWs.send(input);
+    if (socket && input) {
+      socket.send(input);
       setInput('');
     }
   };
 
-  const handleClose = () => {
-    if (isWs) {
-      isWs.close();
-      setisWs(null);
+  const handleConnectWebSocket = () => {
+    if (!socket) {
+      const ws = new WebSocket('ws://localhost:3001');
+      ws.onopen = () => console.log('WebSocket Connected');
+      ws.onmessage = (event) => setOutput(event.data);
+      ws.onclose = () => console.log('WebSocket Disconnected');
+      ws.onerror = (error) => console.error('WebSocket Error:', error);
+
+      setSocket(ws);
+    }
+  };
+
+  const handleCloseWebSocket = () => {
+    if (socket) {
+      socket.close();
+      setSocket(null);
     }
   };
 
   return (
     <div>
-      <div style={{ width: '50%', height: 200, border: `1px solid #000` }}>{output}</div>
+      <h3>웹소켓 연결 : {socket ? '연결됨' : '끊김'}</h3>
+
+      <div style={{ width: '50%', height: 200, border: `1px solid #000`, whiteSpace: 'pre-wrap', overflow: 'auto' }}>
+        {output}
+      </div>
       <form style={{ display: 'flex', gap: 10, padding: '20px 0px' }} onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={handleInputChange} placeholder="node cli..." />
-        <button type="submit">실행</button>
-        <button type="button" onClick={handleClose}>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="node cli..." />
+
+        <button type="submit" disabled={!socket}>
+          Send
+        </button>
+        <button type="button" onClick={handleConnectWebSocket}>
+          웹소켓 연결
+        </button>
+        <button type="button" onClick={handleCloseWebSocket}>
           웹소켓 끊기
         </button>
       </form>
+
+      <code>
+        <div>cd</div>
+        <div>dir</div>
+        <div>type gitignore</div>
+      </code>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { exec } = require('child_process');
+const iconv = require('iconv-lite');
 const WebSocket = require('ws');
 // const db = require('./db');
 const { NODE_PORT, WSS_PORT } = require('./properties');
@@ -13,7 +14,7 @@ const morgan = require('morgan');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ port: WSS_PORT });
+const socket = new WebSocket.Server({ port: WSS_PORT });
 
 const ROOT = path.resolve(__dirname, '../dist');
 const IS_PROD = process.env.NODE_ENV === 'production';
@@ -31,31 +32,31 @@ app.use(
 );
 
 // webSocket
-wss.on('connection', (ws) => {
-  console.log('클라이언트 연결됨');
+socket.on('connection', (ws) => {
+  console.log('socket connection');
 
   ws.on('message', (message) => {
-    if (Buffer.isBuffer(message)) {
-      message = message.toString();
-    }
-    console.log('Received message:', message);
+    if (message && Buffer.isBuffer(message)) {
+      const command = message.toString();
 
-    exec(message, (err, stdout, stderr) => {
-      if (err) {
-        ws.send(`에러: ${stderr}`);
-      } else {
-        ws.send(stdout);
-      }
-    });
+      exec(command, { shell: 'cmd.exe', encoding: 'buffer' }, (err, stdout, stderr) => {
+        if (err) {
+          const result = iconv.decode(stderr, 'euc-kr');
+          ws.send(`Error : ${result}`);
+          return;
+        } else {
+          ws.send(iconv.decode(stdout, 'euc-kr'));
+        }
+      });
+    }
+  });
+  ws.on('close', (code, reason) => {
+    console.log('socket close' + code + ':' + reason);
+  });
+  ws.on('error', (err) => {
+    logger.error(`Error webSocket : ${err}`);
   });
 });
-
-app.use(express.json());
-app.use(express.static(ROOT));
-
-// route
-const routes = require('./routes/product');
-routes.initialize(app);
 
 // webpack
 if (IS_PROD) {
@@ -90,6 +91,13 @@ if (IS_PROD) {
     });
   });
 }
+
+app.use(express.json());
+app.use(express.static(ROOT));
+
+// route
+const routes = require('./routes/product');
+routes.initialize(app);
 
 server.listen(NODE_PORT, () => {
   console.log(`Listening on ${NODE_PORT}`);
