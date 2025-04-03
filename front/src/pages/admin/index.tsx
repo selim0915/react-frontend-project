9df60d275d3420ddb6d8289cad0593af7b1aea8a
srwoo <srwoo@hyundaiht.co.kr>
2025-04-03 11:02:34 +0900

    fix. socket 프론트 입출력 UI 개선
    - 새로운 메시지가 추가될 때, 스크롤을 자동으로 아래 이동
    - input 자동완성 기능, IME 모드 설정 추가
    - 터미널 스타일 적용
    - socket  활성화여부에 따른 버튼 비활성화

--- front/src/pages/admin/index.tsx
+++ front/src/pages/admin/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const MAX_LENGTH = 10000;
 
 const Admin: React.FC = () => {
+  const outputRef = useRef<HTMLDivElement>(null);
   const [input, setInput] = useState('');
   const [output, setOutput] = useState<string[]>([]);
   const [socket, setSocket] = useState<WebSocket | null>(null);
@@ -55,33 +56,62 @@ const Admin: React.FC = () => {
     });
   };
 
+  useEffect(() => {
+    if (outputRef.current) {
+      outputRef.current.scrollTop = outputRef.current.scrollHeight;
+    }
+  }, [output]);
+
   return (
     <div>
       <h3>웹소켓 연결 : {socket ? '연결됨' : '끊김'}</h3>
 
-      <div style={{ width: '50%', height: 200, border: `1px solid #000`, whiteSpace: 'pre-wrap', overflow: 'auto' }}>
+      <div
+        ref={outputRef}
+        style={{
+          width: '50%',
+          height: 300,
+          color: '#fff',
+          backgroundColor: '#000',
+          border: `1px solid #000`,
+          whiteSpace: 'pre-wrap',
+          overflow: 'auto',
+          resize: 'both',
+        }}
+      >
         {output.map((line, index) => (
           <div key={index}>{line}</div>
         ))}
       </div>
       <form style={{ display: 'flex', gap: 10, padding: '20px 0px' }} onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="node cli..." />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="node cli..."
+          list="searchOptions"
+          style={{ imeMode: 'disabled' }}
+        />
+        <datalist id="searchOptions">
+          <option>cd</option>
+          <option>dir</option>
+          <option>type</option>
+        </datalist>
 
         <button type="submit" disabled={!socket}>
           Send
         </button>
-        <button type="button" onClick={handleConnectWebSocket}>
+        <button type="button" onClick={handleConnectWebSocket} disabled={!!socket}>
           웹소켓 연결
         </button>
-        <button type="button" onClick={handleCloseWebSocket}>
+        <button type="button" onClick={handleCloseWebSocket} disabled={!socket}>
           웹소켓 끊기
         </button>
       </form>
 
       <code>
-        <div>cd</div>
-        <div>dir</div>
-        <div>type gitignore</div>
+        <div>cd logs</div>
+        <div>type 2025-04-03.log</div>
       </code>
     </div>
   );
