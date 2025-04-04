3c0564ef9dacf1c772d81da71629ff0b559b1f82
srwoo <srwoo@hyundaiht.co.kr>
2025-04-04 16:01:57 +0900

    feat. git bash 터미널쉘 추가, exit 터미널 종료 키워드 추가

--- server/config/websocket.js
+++ server/config/websocket.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
+const WebSocket = require('ws');
 const { spawn } = require('child_process');
 const iconv = require('iconv-lite');
-const WebSocket = require('ws');
-const logger = require('../config/winton');
+const logger = require('./winston');
 const { WSS_PORT } = require('../properties');
 
 const ENCODING = 'utf-8';
+const GIT_BASH_PATH = 'C:\\Program Files\\Git\\bin\\bash.exe';
+
+const spawnChildProcess = () => {
+  let childProcess = '';
+  if (fs.existsSync(GIT_BASH_PATH)) {
+    childProcess = spawn(GIT_BASH_PATH, ['--login'], { encoding: 'buffer' });
+  } else {
+    childProcess = spawn('cmd.exe', [], { encoding: 'buffer' });
+    childProcess.stdin.write(iconv.encode('chcp 65001\r\n', ENCODING));
+  }
+  return childProcess;
+};
 
 const setupWebSocket = () => {
   const socket = new WebSocket.Server({ port: WSS_PORT });
@@ -12,37 +25,46 @@ const setupWebSocket = () => {
   socket.on('connection', (ws) => {
     logger.info('Connection WebSocket');
 
-    const cmdProcess = spawn('cmd.exe', [], { encoding: 'buffer' });
+    let process = spawnChildProcess();
     let command = '';
 
-    cmdProcess.stdin.write(iconv.encode('chcp 65001\r\n', ENCODING));
+    const initProcess = (shell) => {
+      shell.stdout.on('data', (data) => {
+        const stdout = iconv.decode(data, ENCODING).trimEnd();
+        if (command !== stdout) {
+          ws.send(`${stdout}\n\n`);
+        }
+      });
 
-    cmdProcess.stdout.on('data', (data) => {
-      const stdout = iconv.decode(data, ENCODING).trimEnd();
-      if (command !== stdout) {
-        ws.send(`${stdout}\n`);
-      } else {
-        ws.send(`$ ${stdout}\n`);
-      }
-    });
-
-    cmdProcess.stderr.on('data', (data) => {
-      const stderr = iconv.decode(data, ENCODING).trimEnd();
-      logger.error(`Error WebSocket : ${stderr}`);
-      ws.send(`Error : ${stderr}`);
-    });
+      shell.stderr.on('data', (data) => {
+        const stderr = iconv.decode(data, ENCODING).trimEnd();
+        logger.error(`Error WebSocket : ${stderr}`);
+        ws.send(`Error : ${stderr}`);
+      });
+    };
+    initProcess(process);
 
     ws.on('message', (message) => {
       if (message && Buffer.isBuffer(message)) {
         command = iconv.decode(message, ENCODING).trimEnd();
-        cmdProcess.stdin.write(`${command}\r\n`);
+        ws.send(`$ ${command}\n`);
+
+        if (command === 'exit') {
+          process.kill();
+          process = spawnChildProcess();
+          initProcess(process);
+
+          ws.send(`$ \n`);
+          return;
+        }
+        process.stdin.write(`${command}\r\n`);
       }
     });
 
     ws.on('close', (code, reason) => {
       const reasonText = 'No reason to be delivered';
       logger.info(`Close WebSocket : [${code}] ${reason || reasonText}`);
-      cmdProcess.kill();
+      process.kill();
     });
   });
 
