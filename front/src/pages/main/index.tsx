e21620bee440dff619d50acb162b7c87c4dd726c
srwoo <srwoo@hyundaiht.co.kr>
2025-04-09 15:54:15 +0900

    test: 실시간 현재 시간 표시 기능 테스트 코드 추가

--- front/src/pages/main/index.tsx
+++ front/src/pages/main/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { netflix } from '../../utils/image.import';
 
-const Main: React.FC = () => (
-  <>
-    <h1>Main</h1>
-    <img src={netflix} alt="img" width={100} height={50} />
-  </>
-);
+const Main: React.FC = () => {
+  const [time, setTime] = useState(new Date());
+  const [_time2, setTime2] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function func() {
+    const _a = '1234';
+  }
+
+  return (
+    <>
+      <h1>Main</h1>
+      <span>{time.toLocaleString()}</span>
+      <img src={netflix} alt="img" width={100} height={50} />
+    </>
+  );
+};
 
 export default Main;
