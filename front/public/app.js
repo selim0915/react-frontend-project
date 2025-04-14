0d2ead84798454f6a638e0dacfc81798a2d42971
srwoo <srwoo@hyundaiht.co.kr>
2025-04-14 14:57:43 +0900

    add comments

--- front/public/app.js
+++ front/public/app.js
@@ -87,7 +87,7 @@ function newsDetail() {
   const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id));
 
-  const template = `
+  let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
         <div class="mx-auto px-4">
@@ -115,6 +115,30 @@ function newsDetail() {
       </div>
     </div>
   `;
+
+  function makeComment(comments, called = 0) {
+    const commentString = [];
+
+    for (let i = 0; i < comments.length; i += 1) {
+      commentString.push(`
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>      
+      `);
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
+    }
+
+    return commentString.join('');
+  }
+
+  template = template.replace('{{__comments__}}', makeComment(newsContent.comments));
 
   container.innerHTML = template;
 }
