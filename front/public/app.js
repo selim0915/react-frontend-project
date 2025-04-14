cc14e58b98eedd25debf7c3fb393c91a223fcf9f
srwoo2 <123532880+srwoo2@users.noreply.github.com>
2025-04-14 15:11:01 +0900

    news API로 게시글 페이지 만들기 (#31)
    * add parcel
    
    * add addEventListener
    
    * 문자열로 html 태그 그리기
    
    * code 를 묶어주는 방법은 function이다.
    
    * javsscript router
    
    * add paging
    
    * cdn tailwindcss, font-awesome
    
    * add comments
    
    * 읽은 게시글 표시 추가

---
+++ front/public/app.js
@@ -0,0 +1,184 @@
+/* eslint-disable no-restricted-globals */
+const container = document.getElementById('root');
+
+const ajax = new XMLHttpRequest();
+
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
+const store = {
+  currentPage: 1,
+  feeds: [],
+};
+
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+}
+
+function makeFeeds(feeds) {
+  return feeds.map((feed) => ({
+    ...feed,
+    read: false,
+  }));
+}
+
+function newsFeedList() {
+  let newsFeed = store.feeds;
+  const newsList = [];
+
+  let template = `
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
+        </div>
+      </div>
+      <div class="px-4 pt-4 text-white">{{__total_news_feed__}}</div>
+      <div class="px-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
+    </div>
+  `;
+
+  if (newsFeed.length === 0) {
+    const makeNewsFeed = makeFeeds(getData(NEWS_URL));
+    store.feeds = makeNewsFeed;
+    newsFeed = makeNewsFeed;
+  }
+
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i += 1) {
+    if (!newsFeed[i]) break;
+
+    newsList.push(`
+      <div class="p-6 ${
+        newsFeed[i].read ? 'bg-red-500' : 'bg-white'
+      } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto">
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
+          </div>
+          <div class="text-center text-sm">
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+          </div>
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+          </div>  
+        </div>
+      </div>    
+    `);
+  }
+  const totalPages = Math.ceil(newsFeed.length / 10);
+
+  template = template.replace('{{__total_news_feed__}}', `${store.currentPage} / ${totalPages}`);
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+  template = template.replace(
+    '{{__next_page__}}',
+    store.currentPage < totalPages ? store.currentPage + 1 : store.currentPage,
+  );
+
+  container.innerHTML = template;
+}
+
+function newsDetail() {
+  const id = location.hash.substr(7);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
+    </div>
+  `;
+
+  for (let i = 0; i < store.feeds.length; i += 1) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
+
+  function makeComment(comments, called = 0) {
+    const commentString = [];
+
+    for (let i = 0; i < comments.length; i += 1) {
+      commentString.push(`
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>      
+      `);
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
+    }
+
+    return commentString.join('');
+  }
+
+  template = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+
+  container.innerHTML = template;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeedList();
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7));
+    newsFeedList();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
