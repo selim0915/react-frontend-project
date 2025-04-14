944f689d1de1d51285ae4e7c29fee9e141752bea
srwoo <srwoo@hyundaiht.co.kr>
2025-04-14 14:13:19 +0900

    cdn tailwindcss, font-awesome

--- front/public/app.js
+++ front/public/app.js
@@ -1,6 +1,7 @@
-const ajax = new XMLHttpRequest();
+/* eslint-disable no-restricted-globals */
+const container = document.getElementById('root');
 
-const contaniner = document.getElementById('root');
+const ajax = new XMLHttpRequest();
 
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
@@ -19,49 +20,106 @@ function getData(url) {
 function newsFeedList() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
-  const totalPages = Math.ceil(newsFeed.length / 10);
 
-  newsList.push('<ul>');
+  let template = `
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
+        </div>
+      </div>
+      <div class="px-4 pt-4 text-white">{{__total_news_feed__}}</div>
+      <div class="px-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
+    </div>
+  `;
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i += 1) {
     if (!newsFeed[i]) break;
 
     newsList.push(`
-    <li>
-      <a href="#/show/${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
+      <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto">
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
+          </div>
+          <div class="text-center text-sm">
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+          </div>
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+          </div>  
+        </div>
+      </div>    
     `);
   }
+  const totalPages = Math.ceil(newsFeed.length / 10);
 
-  newsList.push('</ul>');
-  newsList.push(`<div>Page ${store.currentPage} / ${totalPages}</div>`);
-  newsList.push(`
-    <div>
-      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전</a>
-      <a href="#/page/${store.currentPage < totalPages ? store.currentPage + 1 : store.currentPage}">다음</a>
-    </div>  
-  `);
-  contaniner.innerHTML = newsList.join('');
+  template = template.replace('{{__total_news_feed__}}', `${store.currentPage} / ${totalPages}`);
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+  template = template.replace(
+    '{{__next_page__}}',
+    store.currentPage < totalPages ? store.currentPage + 1 : store.currentPage,
+  );
+
+  container.innerHTML = template;
 }
 
 function newsDetail() {
-  // eslint-disable-next-line no-restricted-globals
   const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id));
 
-  contaniner.innerHTML = `
-  <h1>${newsContent.title}</h1>
-  
-  <div>
-    <a href="#/page/${store.currentPage}">목록으로</a>
-  </div>
+  const template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
+    </div>
   `;
+
+  container.innerHTML = template;
 }
 
 function router() {
-  // eslint-disable-next-line no-restricted-globals
   const routePath = location.hash;
 
   if (routePath === '') {
