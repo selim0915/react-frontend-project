83d906c1796a9e7b4261a26b991d056c3dd28545
srwoo <srwoo@hyundaiht.co.kr>
2025-04-14 13:34:04 +0900

    javsscript router

--- front/public/app.js
+++ front/public/app.js
@@ -1,6 +1,6 @@
 const ajax = new XMLHttpRequest();
 
-const contaniner = document.createElement('div');
+const contaniner = document.getElementById('root');
 
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
@@ -12,34 +12,50 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
+function newsFeedList() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+  newsList.push('<ul>');
 
-const ul = document.createElement('ul');
+  for (let i = 0; i < 10; i += 1) {
+    newsList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+    `);
+  }
 
-window.addEventListener('hashchange', () => {
-  // eslint-disable-next-line no-restricted-globals
-  const id = location.hash.substr(1);
-
-  const newsContent = getData(CONTENT_URL.replace('@id', id));
-  const title = document.createElement('h1');
-
-  title.innerHTML = newsContent.title;
-  contaniner.appendChild(title);
-});
-
-for (let i = 0; i < 10; i += 1) {
-  const div = document.createElement('div');
-
-  div.innerHTML = `
-  <li>
-    <a href="#${newsFeed[i].id}">
-      ${newsFeed[i].title} (${newsFeed[i].comments_count})
-    </a>
-  </li>
-  `;
-
-  ul.appendChild(div.firstElementChild); // div.children[0]
+  newsList.push('</ul>');
+  contaniner.innerHTML = newsList.join('');
 }
 
-document.getElementById('root').appendChild(ul);
-document.getElementById('root').appendChild(contaniner);
+function newsDetail() {
+  // eslint-disable-next-line no-restricted-globals
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+  contaniner.innerHTML = `
+  <h1>${newsContent.title}</h1>
+  
+  <div>
+    <a href="#">목록으로</a>
+  </div>
+  `;
+}
+
+function router() {
+  // eslint-disable-next-line no-restricted-globals
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeedList();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
