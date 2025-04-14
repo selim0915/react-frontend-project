466b1a3dc20413dc28a76d2e6b05d3489e66c3a4
srwoo <srwoo@hyundaiht.co.kr>
2025-04-14 13:09:15 +0900

    code 를 묶어주는 방법은 function이다.

--- front/public/app.js
+++ front/public/app.js
@@ -5,10 +5,14 @@ const contaniner = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 
 const ul = document.createElement('ul');
 
@@ -16,10 +20,7 @@ window.addEventListener('hashchange', () => {
   // eslint-disable-next-line no-restricted-globals
   const id = location.hash.substr(1);
 
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
   const title = document.createElement('h1');
 
   title.innerHTML = newsContent.title;
